bbb81f8ba27ae2ec2ffcccf189f5e71dbe44c0ac
KostPhoenix <KostPhoenix@gmail.com>
2022-07-24 14:08:28 +0300

    Add debug

--- src/index.js
+++ src/index.js
@@ -6,10 +6,13 @@ const upload = (job, settings, src, params) => {
     if (!params.callback) throw new Error("Callback url not provided.");
 
     return new Promise((resolve, reject) => {
+        console.log(src);
         let file = fs.createReadStream(src);
         const formData = new FormData();
         formData.append('file', file);
         formData.append('uid', job.uid);
+        console.log(file);
+        console.log(params.callback);
         fetch(params.callback, {
             method: 'POST',
             body: formData
